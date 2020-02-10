b67cf4f1c39b1e5d73ea7a70e7baf20f17850974
Nick Zimakov <nicks.serge@gmail.com>
2020-02-11 00:34:37 +0300

    init

---
+++ LinkedListTask/LinkedListTask/LinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LinkedList {
+    
+    public class LinkedList <T> {
+        
+        private class Node
+        {
+            
+            public T value;
+            public Node nextPtr;
+            
+            public Node(T value, Node nextPtr)
+            {
+                this.value = value;
+                this.nextPtr = nextPtr;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int _size;
+
+        public int size()
+        {
+            return _size;
+        }
+        
+        public void add(T value)
+        {
+            if (head == null)
+            {
+                head = new Node(value, null);
+                tail = head;
+            }
+            else
+            {
+                tail.nextPtr = new Node(value, null);
+                tail = tail.nextPtr;
+            }
+
+            _size++;
+        }
+        
+        public void add(T value, int index){
+            int count = 0;
+            Node it = head;
+            while (count != index - 1){
+                it = it.nextPtr;
+                count++;
+            }
+
+            Node tmpNext = it.nextPtr;
+            it.nextPtr = new Node(value,tmpNext);
+            
+            _size++;
+        }
+
+        public T get(int index)
+        {
+            int count = 0;
+            Node it = head;
+            while (count < index){
+                it = it.nextPtr;
+                count++;
+            }
+            return it.value;
+        }
+
+        public T remove(int index)
+        {
+            int count = 0;
+            Node it = head;
+            while (count != index - 1){
+                it = it.nextPtr;
+                count++;
+            }
+
+            T value = it.nextPtr.value;
+            it.nextPtr = it.nextPtr.nextPtr;
+
+            _size--;
+            return value;
+        }
+
+    }
+}
---
+++ LinkedListTask/LinkedListTask/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using LinkedList;
+using NUnit.Framework;
+
+namespace LinkedListTask
+{
+    [TestFixture]
+    public class Tests
+    {
+        
+        private LinkedList<int> list = new LinkedList<int>();
+
+        public void init()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                list.add(i + 1);
+            }
+        }
+
+        [Test]
+        public void getTest()
+        {
+            Assert.AreEqual(15, list.get(14));
+        }
+        
+        [Test]
+        public void addTest()
+        {
+            init();
+            Assert.AreEqual(78, list.get(77));
+        }
+        
+        [Test]
+        public void removeTest()
+        {
+            init();
+            list.remove(55);
+            Assert.AreEqual(57, list.get(55));
+        }
+    }
+}
